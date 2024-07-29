03fcb420e134d1551da403ef0b0bff15ada10cba
PrabhakarYdv <prabhakarkr802@gmail.com>
2024-07-29 23:17:28 +0530

    Customize Enter OTP

--- app/src/main/java/com/prabhakar/jantagrocery/view/fragment/OTPFragment.kt
+++ app/src/main/java/com/prabhakar/jantagrocery/view/fragment/OTPFragment.kt
@@ -1,6 +1,8 @@
 package com.prabhakar.jantagrocery.view.fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,15 +11,60 @@ import com.prabhakar.jantagrocery.databinding.FragmentOTPBinding
 
 class OTPFragment : Fragment() {
     private lateinit var binding: FragmentOTPBinding
+    private lateinit var userNumber: String
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentOTPBinding.inflate(layoutInflater)
 
-
+        getUserNUmber()
+        customizeEnterOTP()
         return binding.root
 
+    }
+
+    private fun getUserNUmber() {
+        val bundle = arguments
+        userNumber = bundle?.getString("number").toString()
+        binding.displayUserNumber.text = userNumber
+    }
+
+    private fun customizeEnterOTP() {
+        val editTexts = arrayOf(
+            binding.otp1,
+            binding.otp2,
+            binding.otp3,
+            binding.otp4,
+            binding.otp5,
+            binding.otp6
+        )
+
+        for (index in editTexts.indices) {
+            editTexts[index].addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                }
+
+                override fun afterTextChanged(value: Editable?) {
+                    if (value?.length == 1) {
+                        if (index == editTexts.size - 1) {
+                            editTexts[index + 1].requestFocus()
+                        } else if (value?.length == 0) {
+                            if (index > 0) {
+                                editTexts[index - 1].requestFocus()
+                            }
+                        }
+                    }
+                }
+
+            })
+        }
     }
 
 }
