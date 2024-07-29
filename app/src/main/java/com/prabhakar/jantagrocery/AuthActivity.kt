fae58e3e146cb05590371d6eb0e481d49540805b
PrabhakarYdv <prabhakarkr802@gmail.com>
2024-07-29 19:28:24 +0530

    SignIN UI

--- app/src/main/java/com/prabhakar/jantagrocery/AuthActivity.kt
+++ app/src/main/java/com/prabhakar/jantagrocery/AuthActivity.kt
@@ -1,11 +1,14 @@
 package com.prabhakar.jantagrocery
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.prabhakar.jantagrocery.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/prabhakar/jantagrocery/Util.kt
@@ -0,0 +1,5 @@
+package com.prabhakar.jantagrocery
+
+object Util {
+
+}
